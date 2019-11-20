bcfeecaf1f44549e8b94f6124cbf3eefe9719297
lihaifeng <937014599@qq.com>
2019-11-20 17:34:57 +0800

    增加库等文件

---
+++ ORM_TEST/test.py
@@ -0,0 +1,59 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "day15.settings")
+import django
+django.setup()
+from app02 import models
+#查找所有书名里包含金老板的书
+# ret=models.Book.objects.filter(title__icontains='金老板')
+# for i in ret:
+#     print(i.title)
+#查找出版日期是2018年的书
+# ret=models.Book.objects.filter(publish_date__year='2018')
+# print(ret)
+# 查找出版日期是2017年的书名
+# ret=models.Book.objects.filter(publish_date__year='2017')
+# for i in ret:
+#     print(i.title)
+# 查找价格大于10元的书
+# ret=models.Book.objects.filter(price__gt=10)
+# print(ret)
+# 查找价格大于10元的书名和价格
+# ret=models.Book.objects.filter(price__gt=10)
+# for i in ret:
+#     print('书名为：《{}》，价格为：{}元'.format(i.title,i.price))
+# 查找memo字段是空的书
+# ret=models.Book.objects.filter(memo__isnull=True)
+# print (ret)
+
+
+
+# 查找在北京的出版社
+# ret=models.Publisher.objects.filter(city='北京')
+# print(ret)
+# 查找名字以沙河开头的出版社
+# ret=models.Publisher.objects.filter(name__startswith='沙河')
+# print (ret)
+
+
+# 查找“沙河出版社”出版的所有书籍
+# ret=models.Publisher.objects.filter(name='沙河出版社')
+# for i in ret:
+#     ret=models.Book.objects.filter(publisher=i.pk)
+#     print (ret)
+
+
+
+# 查找作者名字里面带“小”字的作者
+# ret=models.Author.objects.filter(name__contains='小')
+# print(ret)
+# 查找年龄大于30岁的作者
+# ret=models.Author.objects.filter(age__gt='30')
+# print(ret)
+# 查找手机号是155开头的作者
+# ret=models.Author.objects.filter(phone__startswith='155')
+# print (ret)
+# 查找手机号是155开头的作者的姓名和年龄
+# ret=models.Author.objects.filter(phone__startswith='155')
+# for i in ret:
+#     print ('作者姓名：{},年龄：{}'.format(i.name,i.age))
+
